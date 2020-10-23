0ed19ac5723dae7e29d545b2f34f3be91310c389
juliasliu <julia5sliu@gmail.com>
2020-10-23 01:56:39 -0700

    Start to modify tab five screen

--- crib-app/screens/ProfileScreen.tsx
+++ crib-app/screens/ProfileScreen.tsx
@@ -23,7 +23,7 @@ import {
 
 import EditScreenInfo from '../components/EditScreenInfo';
 //import { Text, View } from '../components/Themed';
-
+import styles from '../styles/profile';
 
 const POINTS_DATA = [
   {
@@ -62,34 +62,35 @@ const CHORES_DATA = [
 ];
 
 
-export default function TabFiveScreen() {
+export default function ProfileScreen() {
 
   const [tabIndex, setTabIndex] = useState("");
   const handleTabsChange = (index) => {
     setTabIndex(index);
   }
   const pic = require('../assets/images/ramen.jpg');
- 
+
   function Header(props) {
     const name = props.name;
     const id = props.id;
     const points = props.points
     return (
-      <View style={styles.headerContainer}>
-        <View style={styles.headerColumn}>
+      <View style={styles.profileHeader}>
+        <View style={styles.profileCover}>
+        </View>
+        <View style={styles.profile}>
           <Image
               style={styles.userImage}
               source={pic}
           />
-          
           <View style={styles.userNameRow}>
             <Text style={styles.userNameText}>{name}</Text>
           </View>
-          
+
           <View style={styles.userPointsRow}>
             <Text style={styles.userPointsText}>{'@'+id}</Text>
           </View>
-          
+
           <View style={styles.userPointsRow}>
             <Text style={styles.userPointsText}>{'[Coins]'+points}</Text>
           </View>
@@ -126,15 +127,15 @@ export default function TabFiveScreen() {
     <View style={styles.infoContainer}>
       <Text style={styles.infoTitle}>{item.title}</Text>
       <Text style={styles.infoItem}>{item.desc}</Text>
-     
-        <TouchableOpacity 
+
+        <TouchableOpacity
           activeOpacity={0.8}
           onPress={handlePress}
           style={styles.choresButton}
         >
           <Text style={styles.buttonItem}>Mark Done</Text>
         </TouchableOpacity>
-      
+
     </View>
     <View style={styles.timeContainer}>
       <Text style={styles.timeItem}>{item.time}</Text>
@@ -155,7 +156,7 @@ export default function TabFiveScreen() {
           renderItem={renderPointItem}
         />
       </View>
-      
+
     );
   }
 
@@ -169,7 +170,7 @@ export default function TabFiveScreen() {
         />
 
       </View>
-      
+
     );
   }
 
@@ -177,7 +178,7 @@ export default function TabFiveScreen() {
   return (
     <ScrollView style={styles.scroll}>
       <View style={styles.container}>
-        <Header 
+        <Header
           name = "Richardd Mao"
           points = "100"
           id = "HotSyrup"
@@ -193,140 +194,12 @@ export default function TabFiveScreen() {
           textColor='#BDBDBD'
           paddingVertical={18}
         />
-        <ChooseTab 
+        <ChooseTab
           index = {tabIndex}
         />
 
-      
+
       </View>
     </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  scroll: {
-    backgroundColor: '#FFF',
-  },
-
-  //Header containers
-  headerContainer: {
-    alignItems: 'center',
-    backgroundColor:'#5DB075',
-    marginBottom: 10,
-    marginTop: 0,
-    marginLeft: 0,
-    marginRight: 0,
-    width: '100%',
-  },
-  
-  headerColumn: {
-    backgroundColor: 'transparent',
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 10
-  },
-  userImage: {
-    borderColor: '#FFF',
-    borderRadius: 85,
-    borderWidth: 3,
-    height: 170,
-    marginBottom: 15,
-    width: 170,
-  },
-
-  userNameRow: {
-    marginBottom: 10,
-  },
-  userNameText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  userPointsRow: {
-    marginLeft: 40,
-    marginRight: 40,
-  },
-  userPointsText: {
-    fontSize: 18,
-    textAlign: 'center',
-  },
-
-  //List styles
-  listContainer: {
-    flex: 1,
-    backgroundColor: '#FFF',
-    paddingTop: 0,
-    width: '100%',
-   },
-  
-   listRow: {
-    borderColor: '#f1f1f1',
-    borderBottomWidth: 1,
-    flexDirection: 'row',
-    marginLeft: 10,
-    marginRight: 10,
-    paddingTop: 10,
-    paddingBottom: 10,
-  },
-  
-  iconContainer: { //Can be used as a view conatiner or styling the image itself
-    alignItems: 'center',
-    borderColor: '#5DB075',
-    borderRadius: 10,
-    borderWidth: 1,
-    justifyContent: 'center',
-    height: 50,
-    width: 50
-  },
-
-  infoContainer: {
-    flex: 1,
-    paddingLeft: 20,
-    paddingRight: 20,
-    flexDirection: "column",
-    },
-  
-  timeContainer: {
-    flex:1,
-    paddingLeft: 25,
-    paddingRight: 25,
-    flexDirection: "row",
-    alignItems: "baseline",
-    justifyContent: "flex-end"
-  },
-
-  infoTitle: {
-    color: '#1cad61',
-    fontSize: 25,
-    fontWeight: 'bold'
-   },
-
-  infoItem: {
-    paddingLeft: 10,
-    textAlign: 'left',
-    fontSize: 13.5,
-   },
-  buttonItem:{
-    fontSize: 16,
-   },
-  timeItem: {
-    color: 'gray',
-    fontSize: 13.5,
-  },
-
-  choresButton: {
-    flex:0, //fix??? hard coded button width
-    width:100,
-    elevation: 10,
-    backgroundColor: "#E8E8E8",
-    borderRadius: 10, //how rounded it is
-    paddingVertical: 5,
-    paddingHorizontal: 0,
-    alignItems:'center'
-  } 
-});
