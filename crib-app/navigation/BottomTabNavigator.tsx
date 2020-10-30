b3b23f80117a842f867fb91e5a3e36bc919edc84
Richard Mao <richard.mao.123@gmail.com>
2020-10-30 16:57:26 -0700

    added settings navigation

--- crib-app/navigation/BottomTabNavigator.tsx
+++ crib-app/navigation/BottomTabNavigator.tsx
@@ -17,6 +17,7 @@ import ChatScreen from '../screens/Home/ChatScreen';
 
 import FinancesScreen from '../screens/FinancesScreen';
 import ProfileScreen from '../screens/ProfileScreen';
+import SettingsScreen from '../screens/SettingsScreen';
 
 import SignInScreen from '../screens/SignInScreen';
 import RegisterScreen from '../screens/RegisterScreen';
@@ -273,7 +274,16 @@ function ProfileNavigator() {
       <ProfileStack.Screen
         name="ProfileScreen"
         component={ProfileScreen}
-        options={{
+        options={({navigation}) => ({
+          headerRight: () => (
+            <Ionicons
+              size={30}
+              onPress={() => navigation.navigate('SettingsScreen')}
+              name="ios-settings"
+              color={Colors.white}
+              style={{paddingHorizontal: 20}}
+            />
+          ),
           title: 'Profile',
            headerStyle: {
              backgroundColor: Colors.green,
@@ -282,7 +292,13 @@ function ProfileNavigator() {
              borderBottomWidth: 0,
            },
            headerTintColor: '#fff',
-       }}
+       })}
+      />
+      <ProfileStack.Screen
+        name="SettingsScreen"
+        component={SettingsScreen}
+        options={{ headerTitle: 'Settings',
+        headerTintColor: Colors.darkGray, }}
       />
     </ProfileStack.Navigator>
   );
