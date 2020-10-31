52c9d8cdd04467f7f6cb8e4100a72833e99ce7b3
Robin Chu <robinlilychu@berkeley.edu>
2020-10-30 17:13:36 -0700

    added placeholder for chores, fixed spacing for fridge add screens

--- crib-app/screens/ChoresScreen.tsx
+++ crib-app/screens/ChoresScreen.tsx
@@ -1,32 +1,134 @@
-import * as React from 'react';
+import React, { useState } from "react";
+import Icon from '@expo/vector-icons/FontAwesome';
+import { Ionicons } from '@expo/vector-icons';
 import {
-  TouchableOpacity,
-  Dimensions,
+  FlatList,
+  Image,
+  ImageBackground,
+  Linking,
+  Platform,
+  ScrollView,
   StyleSheet,
+  Text,
+  View,
   Button,
-  StatusBar,
-  Image
+  TouchableOpacity,
+  Modal,
 } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import StyledButton from '../components/StyledButton'
-import styles from '../styles/landing';
-import Colors from '../constants/Colors'
+import StyledInput from '../components/StyledInput';
+import StyledButton from '../components/StyledButton';
+import TaskItem from '../components/TaskItem';
 
+import mainStyles from '../styles/main';
+import styles from '../styles/home';
 
-export default function ChoresScreen({ navigation }) {
-  const pic = require('../assets/images/temp_logo.png');
-  return (
+export default class TasksScreen extends React.Component {
 
-    <View style={styles.container}>
-      <Image source={pic} style={styles.logo} />
-      <Text style={styles.title}>Crib</Text>
-      <Text style ={styles.text}>the Grocery and Household App</Text>
-      <StyledButton title="Sign In" color="green" onPress={()=>navigation.navigate('SignInScreen')} style={{width:"60%", marginTop: 20}}/>
-      <StyledButton title="Register" color="orange" onPress={()=>navigation.navigate('RegisterScreen')} style={{width:"60%", marginTop: 20}}/>
+  state = {
+      CHORES_DATA: [
+        {
+          id: 0,
+          type: "Chore",
+          title: 'Trash',
+          desc: "Take out the trash.",
+          time: '8 mins',
+          owner: require('../assets/images/girl.jpg'),
+          checked: false,
+        },
+        {
+          id: 1,
+          type: "Chore",
+          title: 'Vacuum',
+          desc: "Vacuum the living room.",
+          time: '2 hours',
+          owner: require('../assets/images/nature.jpg'),
+          checked: false,
+        },
+        {
+          id: 2,
+          type: "Chore",
+          title: 'Swiffer',
+          desc: "Swiffer the kitchen.",
+          time: '1 week',
+          owner: require('../assets/images/ramen.jpg'),
+          checked: false,
+        },
+      ],
+      SHOPPING_DATA: [
+        {
+          id: 3,
+          type: "Shopping",
+          title: 'Apples',
+          desc: "1 pack",
+          time: '8 mins',
+          owner: require('../assets/images/silhouette.jpg'),
+          checked: false,
+        },
+        {
+          id: 4,
+          type: "Shopping",
+          title: 'Flour',
+          desc: "1 lbs",
+          time: '2 hours',
+          owner: require('../assets/images/ramen.jpg'),
+          checked: false,
+        },
+        {
+          id: 5,
+          type: "Shopping",
+          title: 'Milk',
+          desc: "1 gallon",
+          time: '1 week',
+          owner: require('../assets/images/girl.jpg'),
+          checked: false,
+        },
+      ],
+  }
 
+  setToggleCheckBox = (id, type) => {
+    if (type === "Chore") {
+      var tasks = [...this.state.CHORES_DATA];
+      var task = tasks[tasks.findIndex(elem => elem.id === id)];
+      task.checked = !task.checked;
+      this.setState({CHORES_DATA: tasks})
+    } else if (type === "Shopping") {
+      var tasks = [...this.state.SHOPPING_DATA];
+      var task = tasks[tasks.findIndex(elem => elem.id === id)];
+      task.checked = !task.checked;
+      this.setState({SHOPPING_DATA: tasks})
+    }
+  }
 
-    </View>
-  );
+  renderTaskItem = ({item, type}) => (
+    <TaskItem item={item} toggleChecked={this.setToggleCheckBox.bind(this)} />
+  )
+
+  render() {
+    return (
+      <ScrollView style={[mainStyles.scroll, mainStyles.container]}>
+        <View style={[mainStyles.contentContainer]}>
+          <Text style={mainStyles.title}>Chores</Text>
+          <Text style={mainStyles.subtitle}>Earn points by completing tasks on time. Nudge your housemates if a task is idle!</Text>
+        </View>
+
+        <View style={mainStyles.listContainer}>
+          <View style={mainStyles.listHeader}>
+            <Text style={[mainStyles.listTitle]}>Chores</Text>
+            <View style={mainStyles.row}>
+              <Text style={mainStyles.small} onPress={() => navigation.navigate("FridgeScreen")}>View all</Text>
+              <StyledButton title="Add" size="sm" color="orange" icon="plus" style={{marginLeft: 10}} onPress={() => console.log("Add new hcore")} />
+            </View>
+          </View>
+          <FlatList
+            data={this.state.CHORES_DATA}
+            numColumns={1}
+            renderItem={this.renderTaskItem.bind(this)}
+            keyExtractor={(item, index) => index.toString()}
+          />
+        </View>
+
+      </ScrollView>
+    );
+  }
 }
--- crib-app/screens/Fridge/InventoryAddScreen.tsx
+++ crib-app/screens/Fridge/InventoryAddScreen.tsx
@@ -19,35 +19,37 @@ export default function InventoryAddScreen({ navigation }) {
         <View style = {{flex:0.1, backgroundColor:Colors.green, alignSelf:'stretch', marginBottom:20, alignItems:'flex-start', paddingTop:20}}>
             <Text style={styles.titleText}>Add Item to Inventory</Text>
         </View> 
-        <View style = {[styles.container,{flex:0.8}]}>
+        <View style = {[styles.container,{flex:1}]}>
         <View style={styles.rowContainer}>
             <View style={[styles.fieldContainer, {paddingStart: 20,}]}>
                 <StyledInput type="text" icon = "plus" label="Item" value = "add item" />
             </View>
         </View>
         <View style={styles.rowContainer}>
-            <View style={[styles.fieldContainer, {padding: 20}]}>
+            <View style={[styles.fieldContainer, {paddingStart: 20}]}>
                 <StyledInput type="text" icon = "users" label="Owner" value = "select owner" />
             </View>
 
         </View>
         <View style={styles.rowContainer}>
-            <View style={[styles.fieldContainer, {paddingEnd: 10}]}>
+            <View style={[styles.fieldContainer, {paddingStart: 20,}]}>
                 <StyledInput type="text" label="Quantity" value = "0" />
             </View>
-            <View style={[styles.fieldContainer, {paddingStart: 10}]}>
+            <View style={[styles.fieldContainer, {paddingEnd: 20}]}>
                 <StyledInput type="text" label="Units" value = "set unit abbreviation" />
             </View>
 
         </View>
         <View style={styles.rowContainer}>
-            <View style={[styles.fieldContainer]}>
+            <View style={[styles.fieldContainer, {paddingStart:20}]}>
                 <StyledInput type="text" label="Expiration Date" value = "MM/DD/YYYY"/>
             </View>
-            
         </View>
-            <StyledButton title="Add Item" color="orange" onPress={()=>alert('item added')} size="lg" />
+        <View style={styles.rowContainer}>
+            <StyledButton title="Add Item" color="orange" onPress={()=>alert('item added')} size="lg" style={{width: "50%", height:"50%", marginTop:10}} /> 
+            </View>
         </View>
+
 
     </KeyboardAvoidingView>
   );
